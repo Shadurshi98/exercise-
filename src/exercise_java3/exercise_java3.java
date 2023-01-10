913811b7691a7f6331eb950140f837a49da5c72c
Shadurshi98 <shadurshig@gmail.com>
2023-01-10 09:14:27 +0530

    exercise

---
+++ src/exercise_java3/exercise_java3.java
@@ -0,0 +1,9 @@
+package exercise_java3;
+
+public class exercise_java3 {
+
+	public static void main(String[] args) {
+ System.out.print("hello");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sgic
+ *
+ */
+module exercise_java3 {
+}
